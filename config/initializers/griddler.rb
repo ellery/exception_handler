be7f0c6376abf43a63b4b06cb1e9dad1af40e85b
Ellery J. Durgin <ellery@saidigital.co>
2015-09-13 22:19:25 -0400

    try two re Github issue 4

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -1,3 +1,5 @@
+require email_processor
+
 Griddler.configure do |config|
   config.processor_class = EmailProcessor # CommentViaEmail
   config.processor_method = :process # :create_comment (A method on CommentViaEmail)

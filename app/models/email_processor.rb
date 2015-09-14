381b37785c3ccf4069991642f7bb28227bbd797b
Ellery J. Durgin <ellery@saidigital.co>
2015-09-13 22:57:22 -0400

    added midel

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -2,6 +2,16 @@ class EmailProcessor
   
   def initialize(email)
     @email = email
+        
+    a =  Subject.find_or_initialize_by(:subject => @email.subject, :from => @email.from[:email])
+    
+    if a.messages.count > 0
+      a.messages.create(:message => @email.body)
+    else
+      a.save!
+      a.messages.create(:message => @email.body)
+    end
+    a.save!
   end
 
   def process

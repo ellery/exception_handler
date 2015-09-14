4e820a645a3977d640f70a11a0775d8ddb248d34
Ellery J. Durgin <ellery@saidigital.co>
2015-09-13 22:14:57 -0400

    added email processor

---
+++ app/controllers/email_controller.rb
@@ -0,0 +1,18 @@
+class EmailProcessor
+  def initialize(email)
+    @email = email
+  end
+
+  def process
+        
+    a =  Subject.find_or_initialize_by(:subject => @email.subject, :from => @email.from[:email])
+    
+    if a.messages.count > 0
+      a.messages.create(:message => @email.body)
+    else
+      a.save
+      a.messages.create(:message => @email.body)
+    end
+    a.save
+  end
+end

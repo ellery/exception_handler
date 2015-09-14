32bba3962da6a92d44ed5051f42a230c6f5f98f3
Ellery J. Durgin <ellery@saidigital.co>
2015-09-13 22:11:56 -0400

    added basic message logic

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.autoload_paths << Rails.root.join('lib')
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,6 +15,8 @@ module ExceptionHandler
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.autoload_paths << Rails.root.join('lib')
+    
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]

bfb5ce20d893ec81152604c532d9bbbcd6a9d1d1
Ellery J. Durgin <ellery@saidigital.co>
2015-09-14 01:24:57 -0400

    make that five

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,6 @@
 class SubjectsController < ApplicationController
   before_action :set_subject, only: [:show, :edit, :update, :destroy]
+  before_filter :require_user_signed_in
 
   # GET /subjects
   # GET /subjects.json

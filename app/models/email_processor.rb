6cb43a523c47b60db4d1ed5c4e17cca35ebc939a
Ellery J. Durgin <ellery@saidigital.co>
2015-09-14 00:49:17 -0400

    added slack example

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,13 +5,13 @@ class EmailProcessor
   end
 
   def process
-    a =  Subject.find_or_initialize_by(:subject => @email.subject, :to => @email.to[:email])
+    a =  Subject.find_or_initialize_by(:subject => @email.subject, :to => @email.to)
     
     if a.messages.count > 0
       a.messages.create(:message => @email.body)
     else
       a.save
-      SlackerService.new.send_slack("https://exceptionnotifcation.herokuapp.com/subject/"+ a.id.to_s, @email.to[:token] , @email.subject).deliver
+      SlackerService.new.send_slack("https://exceptionnotifcation.herokuapp.com/subject/"+ a.id.to_s, @email.to , @email.subject).deliver
       a.messages.create(:message => @email.body)
     end
     a.save!

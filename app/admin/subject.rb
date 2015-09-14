a4aa88beea82a16558f93307d49cd9be90b5bb0c
Prelang Builder <builder@prelang.com>
2015-09-14 00:47:24 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Subject' to allow for create, update

--- app/admin/subject.rb
+++ app/admin/subject.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Subject do
+  permit_params :subject, :from, :priority, :to
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters

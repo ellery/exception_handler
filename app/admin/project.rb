83d3da60c4db059fedd43be3991be6a3d3acd150
Prelang Builder <builder@prelang.com>
2015-09-14 00:47:05 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Project' to allow for create, update

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Project do
+  permit_params :title, :email_address
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
